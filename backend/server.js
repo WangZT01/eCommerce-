32424385c2dd40c99fd7a1201cf033f84ee834d0
WangZT <wangzitao9@gmail.com>
2023-09-28 23:28:26 -0400

    add backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
 import products from '../frontend/src/products.mjs';
 
-
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => {
